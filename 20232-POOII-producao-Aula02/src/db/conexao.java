630a8e62c9f2cf04b4005d0f6e3a94d9780a2ccf
Davi Taveira <davitaveira@gmail.com>
2023-08-31 21:44:00 -0300

    aula-02-db

---
+++ 20232-POOII-producao-Aula02/src/db/conexao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Davi
+ */
+public class conexao {
+
+    private static Connection conn = null;
+    
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/poo2-producao", "root", "");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,"Erro: "+ e);
+            }
+        }
+        return conn;
+    }
+
+}
